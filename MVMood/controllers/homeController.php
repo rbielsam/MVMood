cbdb27289fbfb3a9095df0cba0ff8ae95882dea6
kaoutar <kelkouri@institutmvm.cat>
2026-02-09 19:28:23 +0100

    back-end login

---
+++ MVMood/controllers/homeController.php
@@ -0,0 +1,13 @@
+<?php
+require_once "models/homeModel.php";
+
+class homeController{
+    public function index(){
+        if (isset($_POST['logout'])){
+            session_destroy();
+            header("Location: index.php");
+        }
+    }
+}
+
+?>

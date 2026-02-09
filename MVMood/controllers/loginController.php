c8492ce2f63d2b7e6fe0d5d8982801b7168aa1d0
Fran <ffernandez@institutmvm.cat>
2026-02-09 18:47:35 +0100

    añadiendo brach kaoutar

--- MVMood/controllers/loginController.php
+++ MVMood/controllers/loginController.php
@@ -6,6 +6,8 @@ class loginController {
         require "views/login.php";
     }
     public function loginProcess(){
+        $email = $_POST['email'];
+        $password = $_POST['password';]
         $model = new loginModel();
         $email = $_POST['email'];
         $password = $_POST['password'];
@@ -13,8 +15,20 @@ class loginController {
         if ($user){
             $_SESSION['id'] = $user['id'];
             $_SESSION['nickname'] = $user['nickname'];
-            $_SESSION['id'] = $user['id'];
+            $_SESSION['password'] = $user['password'];
+            $_SESSION['rol'] = $user['rol'];
+
+            header("Location: home.php");
+            exit;
         }
+
+        $_SESSION['error']="Usuario o contraseña incorrectos";
+        require "views/login.php";
+    }
+
+    public function logout(){
+        session_destroy();
+        header("Location: login.php");
     }
 }
 

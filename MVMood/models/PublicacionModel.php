c17796bb8f7e8065376955e1dd3d4b74d6f6766f
Patricio Salazar <patricio.victor.2005@gmail.com>
2026-02-15 22:13:24 +0100

    borrados de algunos metodos hechos, falta revision de los modelos

--- MVMood/models/PublicacionModel.php
+++ MVMood/models/PublicacionModel.php
@@ -5,11 +5,26 @@ require_once 'Publicacion.php';
 
 class PublicacionModel {
 	public function getAll(){
-		
+		$db = connect();
+                $stmt = $db->prepare("SELECT * FROM publicacion");
+                $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                $usuarios = [];
+                foreach ($resultado as $fila) {
+                        $usuarios[] = new Usuario($fila);
+                }
+                return $usuarios;
 	}
 	
 	public function getById($id) {
-        	
+        	$db = conectar();
+                $stmt = $db->prepare("SELECT * FROM publicacion WHERE id = :id");
+                $stmt->execute([':id' => $id]);
+                $fila = $stmt->fetch(PDO::FETCH_ASSOC);
+                if (!$fila) {
+                        return null;
+                }
+                $publicacion = new Publicacion($fila);
+                return $publicacion;
         }
 	
 	public function showUserPost(Usuario $usuario) {
--- MVMood/models/UsuarioModel.php
+++ MVMood/models/UsuarioModel.php
@@ -5,7 +5,14 @@ require_once "Usuario.php";
 class UsuarioModel {
 	
 	public function getAll() {
-	
+		$db = connect();
+		$stmt = $db->prepare("SELECT * FROM publicacion");
+		$resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		$usuarios = [];
+		foreach ($resultado as $fila) {
+			$usuarios[] = new Usuario($fila);
+		}
+		return $usuarios;
 	}
 
 	public function getById($id) {
@@ -26,5 +33,13 @@ class UsuarioModel {
 
 	public function cambiarNickname(Usuario $usuario) {
 		
+	}
+
+	public function buscarPorUUID(string $idUnique): {
+    		$sql = "SELECT * FROM usuarios WHERE uuid = :uuid LIMIT 1";
+    		$stmt = $this->db->prepare($sql);
+    		$stmt->execute([':uuid' => $uuid]);
+
+    		return $stmt->fetch(PDO::FETCH_ASSOC);
 	}
 }

637422cef504a19ea7236ce66c0bdf13e9ee3697
rbielsam <rbielsa@institutmvm.cat>
2026-02-27 17:38:09 +0100

    Vista forgotPassword.php

---
+++ MVMood/views/forgotPassword.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>MVMood - Forgot password?</title>
+</head>
+<body>
+    <header>
+        <div class="header-content">
+            <img id="languaje-icon" src="tierra.png" alt="Mode icon" width="32" height="32">
+            <img id="mode-icon" src="modo-oscuro.png" alt="Mode icon" width="32" height="32">
+        </div>
+    </header>
+
+    <div class="div_container">
+
+        <section class="left_section">
+            <img src="imgLogo.png"/>
+        </section>
+
+        <section class="right_section">
+
+            <form action="" method="POST">
+                <input type="text" name="email" placeholder="email" class="imputs"/><br/>
+                <input type="submit" name="reset-password" value="Reset password" class="resetPassword_button"/><br/><br/>
+            </form>
+
+        </section>
+
+    </div>
+
+    <footer>
+        <p>MVMood 2026</p>
+    </footer>
+
+    <script src="script.js"></script>
+
+</body>
+</html>

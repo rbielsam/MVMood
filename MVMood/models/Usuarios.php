be655ef155d6455340c02afce7876b37b51b0091
Kaoutar El Kouri <kelkouri@institut.mvm>
2026-02-27 16:40:17 +0100

    añadir id faltante

---
+++ MVMood/models/Usuarios.php
@@ -0,0 +1,21 @@
+<?php
+
+class Usuario {
+	public $id;
+	public $idUnique;
+	public $email;
+	public $nickname;
+	public $foto;
+	public $password;
+	public $rol;
+
+	public function __construct($datos = []) {
+		$this->id	= $datos['id'] ?? null;
+		$this->idUnique	= $datos['idUnique'] ?? '';
+		$this->email	= $datos['email'] ?? '';
+		$this->nickname	= $datos['nickname'] ?? '';
+		$this->foto	= $datos['foto'] ?? 'user.png';
+		$this->password	= $datos['password'] ?? '';
+		$this->rol	= $datos['rol'] ?? 'user';
+	}
+}

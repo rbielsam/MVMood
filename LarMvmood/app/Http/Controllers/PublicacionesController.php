fedd188146bea028afdf7d3f15de7f7324c75308
Kaoutar El Kouri <kelkouri@institut.mvm>
2026-03-18 17:08:51 +0100

    vendor?

--- LarMvmood/app/Http/Controllers/PublicacionesController.php
+++ LarMvmood/app/Http/Controllers/PublicacionesController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Publicacion;
 use Illuminate\Http\Request;
 
 class PublicacionesController extends Controller
 {
-    //
+    public function index() {
+        $todasLasPubicaciones = Publicacion::all();
+
+        // Enviem les dades a la vista (com el ModelAndView)
+        return view('publicaciones.home', ['publicaciones' => $todasLasPubicaciones]);
+
+    }
 }
---
+++ LarMvmood/resources/views/publicaciones/home.blade.php
@@ -0,0 +1,7 @@
+@forelse($publicaciones as $publicacion)
+
+@empty
+    <div class="post">
+        <p>No hay publicaciones todavía. ¡Sé el primero en publicar!</p>
+    </div>
+@endforelse

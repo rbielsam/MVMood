45ed9789cf18f4d89b81f85534b3caf4229e9312
Kaoutar El Kouri <kelkouri@institut.mvm>
2026-03-21 09:58:03 +0100

    el modelo Publicacion

--- LarMvmood/app/Models/Publicacion.php
+++ LarMvmood/app/Models/Publicacion.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Publicacion extends Model
 {
-    //
+    protected $table = 'publicacion';
+    protected $primaryKey = 'idUnique';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $fillable = [
+        'idUnique',
+        'idUsuario',
+        'contenido',
+        'foto',
+    ];
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'idUsuario', 'idUnique');
+    }
 }

8a73b97814978d5021eff8354dfc158e274340a4
Patricio Salazar <patricio.victor.2005@gmail.com>
2026-03-23 19:52:22 +0100

    Volvi a crear los models con la opcion -m para que tambien se crearan los archivos de /migrations

--- LarMvmood/app/Models/Comentario.php
+++ LarMvmood/app/Models/Comentario.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comentario extends Model
 {
-    //
+    protected $table = 'comentario';
+    protected $primaryKey = 'idUnique';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $fillable = [
+        'idUnique',
+        'idUsuario',
+        'idPublicacion',
+        'contenido'
+    ];
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'idUsuario', 'idUnique');
+    }
 }
--- LarMvmood/app/Models/Reaccion.php
+++ LarMvmood/app/Models/Reaccion.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reaccion extends Model
 {
-    //
+    protected $table = 'reaccion';
+    protected $primaryKey = 'idUnique';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $fillable = [
+        'idUnique',
+        'idUsuario',
+        'idPublicaion',
+        'reaccion'
+    ];
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'idUsuario', 'idUnique');
+    }
 }

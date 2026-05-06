69440b5baf48b0f0b33259c3f9cf124f7b2d15cc
Kaoutar El Kouri <kelkouri@institut.mvm>
2026-05-06 17:23:07 +0200

    borrando contenido de web.php

--- mvmood/routes/web.php
+++ mvmood/routes/web.php
@@ -1,52 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\PublicacionesController;
-use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\Auth\RegisterController;
+
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::middleware('guest')->group(function () {
-    Route::get('/amvmood', [LoginController::class, 'showLogin']);
-    Route::post('/login', [LoginController::class, 'login']);
-    Route::get('/register', [RegisterController::class, 'showRegistrationForm']);
-    Route::post('/register', [RegisterController::class, 'register']);
-});
-
-Auth::routes(['verify' => true]);
-
-Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
-
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
-
-    return redirect('/home');
+    return redirect(env('FRONT_URL', 'http://localhost:5173').'/login?verified=1');
 })->middleware(['auth', 'signed'])->name('verification.verify');
-
-
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
-
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-Route::post('/logout', [LogoutController::class, 'logout']);
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/home', [PublicacionesController::class, 'index'])->name('publicaciones.home');
-    Route::get('/publicacion/crear', [PublicacionesController::class, 'crear']);
-    Route::post('/publicacion/guardar', [PublicacionesController::class, 'store']);
-    Route::get('/publicacion/editar/{id}', [PublicacionesController::class, 'editar']);
-    Route::post('/publicacion/update', [PublicacionesController::class, 'update']);
-    Route::get('/publicacion/eliminar/{id}', [PublicacionesController::class, 'eliminar']);
-    Route::get('/perfil/editar/{id}', [UserController::class, 'editarPerfil']);
-    Route::post('/perfil/update', [UserController::class, 'updatePerfil']);
-});
-
-

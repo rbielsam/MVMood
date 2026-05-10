73cb6900514c8cd97345315c82d49f491241ff52
rbielsam <rbielsa@institutmvm.cat>
2026-05-10 11:21:51 +0200

    Arreglos Chat - Continuamos...

--- mvmood/routes/channels.php
+++ mvmood/routes/channels.php
@@ -2,13 +2,16 @@
 use App\Models\Chat;
 use Illuminate\Support\Facades\Broadcast;
 
+
+Broadcast::routes(['middleware' => ['auth:sanctum']]);
+
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('chat.{chatUuid}', function ($user, $chatUuid) {
+Broadcast::channel('chat.{chatId}', function ($user, $chatId) {
     // IMPORTANTE: $user es el usuario autenticado actualmente
-    return Chat::where('uuid', $chatUuid)
+    return Chat::where('id', $chatId)
                ->whereHas('usuarios', function($q) use ($user) {
                    $q->where('user_id', $user->id);
                })->exists();

e40ec9d4e38af86202d93b40fd4a10b83310cf37
Patricio Salazar <patricio.victor.2005@gmail.com>
2026-05-15 18:33:01 +0200

    delete update

--- mvmood/app/Http/Controllers/Api/UserController.php
+++ mvmood/app/Http/Controllers/Api/UserController.php
@@ -103,7 +103,7 @@ class UserController extends Controller
         $user->delete();
 
         return response()->json([
-            'message' => 'Usuario eliminado'
+            null
         ], 204);
     }
  }

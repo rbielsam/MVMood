e8c20f1ce365c7310e785c75a4c0beefa1179bfa
rbielsam <rbielsa@institutmvm.cat>
2026-05-15 20:15:06 +0200

    Arreglos función delete Acount user

--- mvmood/app/Http/Controllers/Api/UserController.php
+++ mvmood/app/Http/Controllers/Api/UserController.php
@@ -103,7 +103,7 @@ class UserController extends Controller
         $user->delete();
 
         return response()->json([
-            null
-        ], 204);
+            'message' => 'Cuenta eliminada'
+        ], 200);
     }
  }
